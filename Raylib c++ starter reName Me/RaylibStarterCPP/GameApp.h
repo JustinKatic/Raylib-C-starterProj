28f78d455005cfd98bf5ae70e3dec9a15560a2bc
Justin <justinkatic4@gmail.com>
2020-07-03 11:43:03 +1000

    no message

--- Raylib c++ starter reName Me/RaylibStarterCPP/GameApp.h
+++ Raylib c++ starter reName Me/RaylibStarterCPP/GameApp.h
@@ -5,18 +5,14 @@ public:
 	GameApp();
 	~GameApp();
 
-	bool gameOver = false;
-	static const int MAP_ROWS = 32;
-	static const int MAP_COLS = 50;
-
 	void Startup();
 	void Shutdown();
 	void Run();
 
 	void Update();
 	void Draw();
-private:
 
+private:
 	int m_screenWidth;
 	int m_screenHeight;
 
